f5a946ab27d08ee9931bc7b013fd9bb56d430b2f
MisterMeme01 <69019876+MisterMeme01@users.noreply.github.com>
2022-09-14 04:30:09 -0400

    Arguments Added
    This is just a test case to ensure that I'm using sys.argv properly.

--- client.py
+++ client.py
@@ -2,7 +2,15 @@
 
 import sys
 
-if __name__ == '__main__':
-    sys.stderr.write("client is not implemented yet\n")
+#if __name__ == '__main__':
+#    sys.stderr.write("client is not implemented yet\n")
 
-#Testing, testing!
+#This is simply a way for us to run a test to see if
+#We're passing the files correct
+
+args_length = len(sys.argv)
+print("There are {arguments} in the list".format(arguments=args_length))
+i = 0
+while i < args_length:
+    print("Argument #{args} = {argu}".format(args=i, argu=sys.argv[i]))
+    i+= 1
