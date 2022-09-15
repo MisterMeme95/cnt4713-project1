9b2542ac80f430d10425429cc77c23f90009cc41
MisterMeme01 <69019876+MisterMeme01@users.noreply.github.com>
2022-09-14 22:29:10 -0400

    Adding Connection Function
    Adding a function that allows for a connection to be established.

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import socket
 
 #if __name__ == '__main__':
 #    sys.stderr.write("client is not implemented yet\n")
@@ -8,9 +9,38 @@ import sys
 #This is simply a way for us to run a test to see if
 #We're passing the files correct
 
-args_length = len(sys.argv)
-print("There are {arguments} in the list".format(arguments=args_length))
-i = 0
-while i < args_length:
-    print("Argument #{args} = {argu}".format(args=i, argu=sys.argv[i]))
-    i+= 1
+
+#These variables will be used for connectivity.
+def makeConnection(connection, host, port):
+    port = int(sys.argv[2])
+    try:
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    except socket.error as err:
+        sys.stderr.write("ERROR - Socket Creation failed!")
+        return False
+
+    try:
+        host = socket.gethostbyname(sys.argv[1])
+
+    except socket.gaierror:
+        sys.stderr.write("ERROR - The host could not be reached!")
+        return False
+
+
+    try:
+        connection.connect((host, port))
+        data = connection.recv(1024)
+        print("Received. . . {dater}".format(dater=data))
+
+    except:
+        sys.stderr.write("ERROR - A connection could not be established!")
+        return False
+        #data = s.recv(1024)
+        #print('Received', repr(data))
+
+
+domain_name = "blank"
+port = 0
+client_cpu = 0
+makeConnection(client_cpu, domain_name, port)
