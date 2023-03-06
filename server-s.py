d61e6fd0c0d7924706a942c6ad2ae16476a66966
MisterMeme01 <jonathanmathurin6@gmail.com>
2023-03-05 23:56:40 -0500

    d

--- server-s.py
+++ server-s.py
@@ -2,13 +2,11 @@ import socket
 import sys
 import signal
 import time
-import selectors
-selector = selectors.DefaultSelector()
 
 # define signal handler function
 def signal_handler(signal, frame):
     global not_stopped
-    #print('Exiting gracefully...')
+    print('Exiting gracefully...')
     not_stopped = False
     sys.exit(0) # exit with code 0
 
@@ -44,20 +42,15 @@ def server_program():
     host = socket.gethostname()
     port = int(sys.argv[1])  # initiate port no above 1024
 
-    server_socket = socket.socket()  # get instance
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
     # look closely. The bind() function takes tuple as argument
     server_socket.bind((host, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-
     server_socket.listen(10)
-    server_socket.setblocking(False)
-    selector.register(server_socket, selectors.EVENT_READ)
+    client_socket, server_address = server_socket.accept()  # accept new connection
 
-    conn, address = server_socket.accept()  # accept new connection
-    conn.settimeout(10)
-    conn.setblocking(False)
-    selector.register(conn, selectors.EVENT_READ)
+    client_socket.settimeout(10)
 
         # send accio command to the client
     #conn.send(b'accio\r\n')
@@ -66,16 +59,27 @@ def server_program():
     #print("Connection from: " + str(address))
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
+        data = client_socket.recv(1024)#.decode()
         if not data:
             # if data is not received break
             break
         #print("from connected user: " + str(data))
         #data = input(' -> ')
-        conn.send(b'accio\r\n')
+
+    client_socket.send(b'accio\r\n')
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = client_socket.recv(1024)#.decode()
+        if not data:
+            # if data is not received break
+            break
+        #print("from connected user: " + str(data))
+        #data = input(' -> ')
+
+    client_socket.send(b'accio\r\n')
         #conn.send(data.encode())  # send data to the client
 
-    conn.close()  # close the connection
+    client_socket.close()  # close the connection
 
 
 if __name__ == '__main__':
