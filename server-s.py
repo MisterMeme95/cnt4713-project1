d3ae2cf21a30dfd105034db6a3332d9ec0da4f23
MisterMeme01 <jonathanmathurin6@gmail.com>
2023-03-05 21:46:09 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -44,7 +44,7 @@ def server_program():
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
     # look closely. The bind() function takes tuple as argument
-    server_socket.bind(('0.0.0.0', port))  # bind host address and port together
+    server_socket.bind((0.0.0.0, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
     server_socket.listen(10)
@@ -59,7 +59,7 @@ def server_program():
     #print("Connection from: " + str(address))
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
+        data = conn.recv(1024)#.decode()
         if not data:
             # if data is not received break
             break
