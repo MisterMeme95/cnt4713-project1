ffbf2df282b2e849071388c05a7fdb2c85abba30
MisterMeme01 <jonathanmathurin6@gmail.com>
2023-03-05 23:20:02 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -65,7 +65,18 @@ def server_program():
             break
         #print("from connected user: " + str(data))
         #data = input(' -> ')
-        conn.send(b'accio\r\n')
+
+    conn.send(b'accio\r\n')
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024)#.decode()
+        if not data:
+            # if data is not received break
+            break
+        #print("from connected user: " + str(data))
+        #data = input(' -> ')
+
+    conn.send(b'accio\r\n')
         #conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
